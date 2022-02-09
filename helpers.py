7162f68b399b69c7018d63479c99e326f49434c0
Jen Nerby <nerby.jennifer@gmail.com>
2022-02-09 11:45:12 -0500

    added post sorting

--- helpers.py
+++ helpers.py
@@ -2,8 +2,11 @@ import requests
 
 API = 'https://api.hatchways.io/assessment/blog/posts'
 
-def ping_api_response():
+def ping_api_response(tag):
     """Returns api call response"""
-    payload = {'tag': 'all'}
+    payload = {'tag': tag}
     res = requests.get(API, params=payload)
     return res
+
+#### TODO ####
+# def get_tags_from_url(url)
