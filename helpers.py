52f1b28d4caababda6d8e7508fcb2643e38f8394
Jen Nerby <nerby.jennifer@gmail.com>
2022-02-09 12:47:38 -0500

    modularized parts 1-2

--- helpers.py
+++ helpers.py
@@ -2,11 +2,45 @@ import requests
 
 API = 'https://api.hatchways.io/assessment/blog/posts'
 
-def ping_api_response(tag):
+def get_tag_responses(tags: list) -> list:
+    tag_responses = []    
+    # call api for each tag and push to a set - SET COMP NOT WORKING
+    for tag in tags:
+        res = ping_api_response(tag).json()
+        tag_responses.extend(res['posts'])
+
+    return remove_duplicate_posts(tag_responses)
+
+## TYPE INDICATOR FOR RESPONSE?
+def ping_api_response(tag: str):
     """Returns api call response"""
     payload = {'tag': tag}
     res = requests.get(API, params=payload)
     return res
+
+
+def remove_duplicate_posts(tag_responses: list) -> list:
+    """Removes duplicate posts in tag_responses list"""
+    # init set for unique post ids
+    api_calls_ids = set()
+    # empty list for unique results
+    result = []
+
+    # remove duplicate calls
+    for response in tag_responses:
+        if response['id'] not in api_calls_ids:
+            result.append(response)            
+            api_calls_ids.add(response['id'])
+
+    return result
+
+def sort_result(result: list, sort_by_value: str, sort_direction: str) -> list:
+    """Sorts responses"""
+    if sort_direction == 'desc':
+        sorted_result = sorted(result, key=lambda i: i[sort_by_value], reverse=True)
+    else:
+        sorted_result = sorted(result, key=lambda i: i[sort_by_value])
+    return sorted_result
 
 #### TODO ####
 # def get_tags_from_url(url)
--- server.py
+++ server.py
@@ -20,42 +20,22 @@ def ping_api():
 @app.route('/api/posts')
 def return_api_posts():
     """Returns all posts with at least one tag specified"""
+    
     #### WHERE DOES URL COME FROM _ HOW TO SPLIT TAGS???
     # get list of tags user passed in
     tags = ['tech', 'history', 'health']
-
-    # empty list for all tag calls including dupes
-    tag_calls = []
     
-    # init set for unique post ids
-    api_calls_ids = set()
-    # empty list for unique results
-    result = []
+    # get unique 
+    result = helpers.get_tag_responses(tags)
 
-    # call api for each tag and push to a set - SET COMP NOT WORKING
-    for tag in tags:
-        res = helpers.ping_api_response(tag).json()
-        tag_calls.extend(res['posts'])
-
-    # remove duplicate calls
-    for call in tag_calls:
-        if call['id'] not in api_calls_ids:
-            result.append(call)            
-            api_calls_ids.add(call['id'])
-
-    # sort results
     ### HOW TO EXTRACT SORT PREFERENCES
-    sort_by_value = 'likes'
+    sort_by_value = 'popularity'
     sort_direction = 'desc'
+
     if not sort_direction:
         return jsonify(result)
     
-    if sort_direction == 'desc':
-        sorted_result = sorted(result, key=lambda i: i[sort_by_value], reverse=True)
-    else:
-        sorted_result = sorted(result, key=lambda i: i[sort_by_value])
-
-    return jsonify(sorted_result)
+    return jsonify(helpers.sort_result(result, sort_by_value, sort_direction))
 
 if __name__ == '__main__':
     app.run(debug=True)
